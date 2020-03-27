3b57d1bf5d5bae39a0b059d13393c60703d78e30
Mark Hoffmann <grizzo@gmail.com>
2020-03-27 09:07:30 -0700

    version 0.2.0

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # wp-otis
 WordPress plugin for the Oregon Tourism Information System
+
+Version 0.2.0
--- wp-otis.php
+++ wp-otis.php
@@ -7,7 +7,7 @@
  * Author URI:      thinkshout.com
  * Text Domain:     wp-otis
  * Domain Path:     /languages
- * Version:         0.1.0
+ * Version:         0.2.0
  *
  * @package         Otis
  */
