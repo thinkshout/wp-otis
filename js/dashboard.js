3386b0754a1e143ced44ab3af45cc61d9fb36e08
jamacon36 <4700339+jamacon36@users.noreply.github.com>
2021-07-13 18:29:08 -0400

    adding statuses to dashboard

--- js/dashboard.js
+++ js/dashboard.js
@@ -4,6 +4,20 @@
 		template: `
       <div class="otis-dashboard">
         <h1>OTIS Dashboard</h1>
+        <div class="otis-dashboard__statuses">
+          <div class="otis-dashboard__status">
+            <p>Last Import</p>
+            <p>{{ lastImport }}</p>
+          </div>
+          <div class="otis-dashboard__status">
+            <p>Bulk Importer Active</p>
+            <p>{{ importerActive }}</p>
+          </div>
+          <div class="otis-dashboard__status">
+            <p>Bulk History Importer Active</p>
+            <p>{{ historyImporterActive }}</p>
+          </div>
+        </div>
         <div class="otis-dashboard__settings">
           <div class="input-text-wrap">
             <label for="modified-date">Import Modified POIs Since Date</label>
@@ -21,12 +35,35 @@
     `,
 		data: {
 			fromDate: "",
+			lastImportDate: "",
+			bulkImportActive: "",
+			bulkHistoryImportActive: "",
+		},
+		computed: {
+			lastImport() {
+				if (!this.lastImportDate) return "N/A";
+				const lastImportDate = new Date(this.lastImportDate);
+				return `${lastImportDate.getMonth()}/${lastImportDate.getDate()}/${lastImportDate.getFullYear()} @ ${lastImportDate.getHours()}:${lastImportDate.getMinutes()}`;
+			},
+			importerActive() {
+				return this.bulkImportActive ? "Active" : "Inactive";
+			},
+			historyImporterActive() {
+				return this.bulkHistoryImportActive ? "Active" : "Inactive";
+			},
 		},
 		methods: {
+			async otisStatus() {
+				const payload = new FormData();
+				payload.append("action", "otis_status");
+				const { data } = await axios.post(otisDash.ajax_url, payload);
+				Object.keys(data).forEach((key) => {
+					this[key] = data[key];
+				});
+			},
 			triggerImport() {
 				if (
-					this.fromDate &&
-					this.fromDate.match(
+					this.fromDate?.match(
 						/^\d{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])$/g
 					)
 				) {
@@ -35,6 +72,9 @@
 					console.log("No match");
 				}
 			},
+		},
+		async mounted() {
+			await this.otisStatus();
 		},
 	});
 })();
--- src/Otis_Dashboard.php
+++ src/Otis_Dashboard.php
@@ -7,9 +7,11 @@ class Otis_Dashboard
     wp_register_script( 'vue', 'https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js', [], '2.6.14' );
     wp_enqueue_script( 'axios' );
     wp_enqueue_script( 'vue' );
+    wp_register_script( 'otis-dashboard', plugins_url( '../js/dashboard.js', __FILE__ ), [], '1.0', true );
+    wp_localize_script( 'otis-dashboard', 'otisDash', array( 'ajax_url' => admin_url( 'admin-ajax.php' ) ) );
 
+    wp_enqueue_script( 'otis-dashboard' );
     wp_enqueue_style( 'otis-dashboard-styles', plugins_url( '../css/dashboard.css', __FILE__ ), [], '1.0' );
-    wp_enqueue_script( 'otis-dashboard', plugins_url( '../js/dashboard.js', __FILE__ ), [], '1.0', true );
   }
 
   public function otis_dashboard_page() {
@@ -25,10 +27,27 @@ class Otis_Dashboard
   public function otis_dashboard_setup() {
     $this->otis_dashboard_ui();
   }
+
+  public function otis_start_import() {
+
+  }
+
+  public function otis_status() {
+    echo json_encode([
+      'bulkImportActive' => WP_OTIS_BULK_IMPORT_ACTIVE,
+      'bulkHistoryImportActive' => WP_OTIS_BULK_HISTORY_ACTIVE,
+      'lastImportDate' => WP_OTIS_LAST_IMPORT_DATE,
+    ]);
+    wp_die();
+  }
   
   function __construct() {
     add_action( 'admin_menu', [ $this, 'otis_dashboard_page' ] );
     add_action( 'admin_enqueue_scripts', [ $this, 'otis_dashboard_scripts' ] );
+
+    // Ajax Handlers
+    add_action( 'wp_ajax_otis_status', [ $this, 'otis_status' ] );
+    add_action( 'wp_ajax_otis_import', [ $this, 'otis_start_import' ] );
   }
 }
 
