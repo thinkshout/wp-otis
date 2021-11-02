f06283a6932fa30e3e5ed92e732ee5165f6aaff0
James Macon <4700339+jamacon36@users.noreply.github.com>
2021-11-02 14:07:49 -0400

    removing bulk history import param

--- src/Otis_Importer.php
+++ src/Otis_Importer.php
@@ -551,6 +551,7 @@ class Otis_Importer {
 					}
 				}
 
+				// Run through the post ids in history_deletes and trash the posts.
 				foreach ($history_deletes as $post_id) {
 					$this->logger->log('Trashing post ' . $post_id);
 					wp_trash_post($post_id);
@@ -562,7 +563,7 @@ class Otis_Importer {
 					if ($assoc_args['bulk-history-page'] < $history_page_count) {
 						$assoc_args['bulk-history-page'] = $assoc_args['bulk-history-page'] + 1;
 						$this->logger->log('Enqueueing history import page ' . $assoc_args['bulk-history-page']);
-						as_enqueue_async_action('wp_otis_async_bulk_history_import', ['params' => ['all' => $assoc_args['all'], 'page' => $assoc_args['bulk-history-page'], 'bulk-history-page' => $assoc_args['bulk-history-page'], 'modified' => $assoc_args['modified'], 'related_only' => $assoc_args['related_only']]]);
+						as_enqueue_async_action('wp_otis_async_bulk_history_import', ['params' => ['all' => $assoc_args['all'], 'page' => $assoc_args['bulk-history-page'], 'modified' => $assoc_args['modified'], 'related_only' => $assoc_args['related_only']]]);
 					} elseif ($assoc_args['bulk-history-page'] == $history_page_count) {
 						update_option(WP_OTIS_BULK_HISTORY_ACTIVE, false);
 						delete_transient(WP_OTIS_BULK_IMPORT_TRANSIENT);
