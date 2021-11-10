9a97e3aa50e0c8b6ab8b1cd3348683100ddb6d0b
James Macon <4700339+jamacon36@users.noreply.github.com>
2021-11-10 13:38:12 -0500

    logging POIs as they're modified

--- src/Otis_Importer.php
+++ src/Otis_Importer.php
@@ -1016,10 +1016,7 @@ class Otis_Importer {
                 throw new Otis_Exception('Error: taxonomy glocats, post id ' . $post_id . ', ' . $return->get_error_message());
             }
 
-            $bulking = get_option(WP_OTIS_BULK_IMPORT_ACTIVE, false);
-            if (!$bulking) {
-                $this->logger->log(ucfirst($upsert_status) . ' POI with UUID: ' . $result['uuid'], $post_id);
-            }
+            $this->logger->log(ucfirst($upsert_status) . ' POI with UUID: ' . $result['uuid'], $post_id);
 
             return $post_id;
         }
