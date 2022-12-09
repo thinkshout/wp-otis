2c6c78a53beb5e802174aadacb00ebcb3773f0db
James Macon <4700339+jamacon36@users.noreply.github.com>
2022-12-09 14:45:09 -0500

    adds default color defs to dashboard

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -7,6 +7,19 @@ import { createVuestic } from 'vuestic-ui';
 export default function dashboardVue() {
 	const app = createApp(OtisDashboard);
 	app.component('Datepicker', Datepicker);
-	app.use(createVuestic());
+	app.use(createVuestic({
+		config: {
+			colors: {
+				primary: '#23e066',
+				secondary: '#002c85',
+				success: '#40e583',
+				info: '#2c82e0',
+				danger: '#e34b4a',
+				warning: '#ffc200',
+				gray: '#babfc2',
+				dark: '#34495e',
+			}
+		}
+	}));
 	app.mount("#otis-dashboard-mount");
 };
